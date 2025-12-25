9a98e1e6ebe733a8e860ba3b2acffe27d9d3d5f0
Anrijs <anrijs@anrijs.lv>
2025-12-25 07:07:17 +0200

    logger: fix build

--- examples/simple_web_logger/main.cpp
+++ examples/simple_web_logger/main.cpp
@@ -836,7 +836,6 @@ protected:
 
       addHistory(msgData);
     }
-    }
   }
 
   uint8_t onContactRequest(const ContactInfo& contact, uint32_t sender_timestamp, const uint8_t* data, uint8_t len, uint8_t* reply) override {
